f867a818bd91bda2e7add73885bec10d457a6f87
akapapillio <pixelshinobu@gmail.com>
2026-03-30 11:12:09 +0000

    refractor(db) update

---
+++ vaovao-frhoshi/docker/mysql/update-table-30-3-2026.sql
@@ -0,0 +1,6 @@
+
+USE news_db0000;
+ALTER TABLE admins ADD COLUMN nom VARCHAR(50) NOT NULL AFTER id;
+UPDATE admins 
+SET nom = 'Admin' 
+WHERE email = 'admin@mail.com';
---
+++ vaovao-frhoshi/docker/mysql/workflowDB.md
@@ -0,0 +1,3 @@
+docker exec -it <news_mysql0000> bash
+mysql -u root -p
+mdp : root
